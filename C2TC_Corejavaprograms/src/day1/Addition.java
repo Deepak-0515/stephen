c280063376bb2f26585001c3253c58d072c30c33
ACT-C2-02 <ACT-C2-02@DESKTOP-I4SCAC5>
2025-06-13 10:33:10 +0530

    additional java program

---
+++ C2TC_Corejavaprograms/src/day1/Addition.java
@@ -0,0 +1,16 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b =15;
+		int total = a+b;
+		System.out.println(total);
+
+	}
+
+}
